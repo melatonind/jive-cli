d13bc63b68d0d7b550e08f38d43077fa974989ea
mei.brough-smyth <mei.brough-smyth@rea-group.com>
2016-05-19 00:13:44 +1000

    POC complete.

--- modify_document.sh
+++ modify_document.sh
@@ -20,17 +20,42 @@ function set_password {
 }
 
 function get_content_id {
-  echo "Waiting..."
-  CONTENT_ID=curl -u "$USER_ID":"$USER_PW" "${JIVE_ENDPOINT}contents?filter=entityDescriptor(102,${DOC_ID})" | tail -n +2 | jq -r .list[].contentID
-  echo $CONTENT_ID
+  echo "Retrieving content ID..."
+  CONTENT_ID=$(curl -u "$USER_ID":"$USER_PW" "${JIVE_ENDPOINT}contents?filter=entityDescriptor(102,${DOC_ID})" | tail -n +2 | jq -r .list[].contentID)
 }
 
 function load_document {
-  echo "Downloading..."
-  curl -u "$USER_ID":"$USER_PW" "${JIVE_ENDPOINT}contents/${CONTENT_ID}" > tmp.txt
+  echo "Retrieving DOC-${DOC_ID}..."
+  curl -u "$USER_ID":"$USER_PW" "${JIVE_ENDPOINT}contents/$CONTENT_ID" | tail -n +2 > tmp.txt
+  SUBJECT=$( cat tmp.txt | jq .subject )
+  CONTENT=$( cat tmp.txt | jq .content.text )
+  echo $CONTENT > tmp.txt
+}
+
+function edit_document {
+  vim tmp.txt < `tty` > `tty`
+  CONTENT=$( cat tmp.txt )
+}
+
+function update_document {
+  curl -s -u "$USER_ID":"$USER_PW" -X PUT \
+     -k --header "Content-Type: application/json" \
+     -d '{ "subject": '"${SUBJECT}"',
+           "type": "document",
+           "status" : "published",
+           "tags" : [ ],
+           "content":
+              { "type": "text/html",
+                "text": '$CONTENT'
+              }
+         }' \
+     "${JIVE_ENDPOINT}contents/95349"
+  echo "Uploaded DOC-${DOC_ID}"
 }
 
 set_login
 set_password
 get_content_id
 load_document
+edit_document
+update_document
