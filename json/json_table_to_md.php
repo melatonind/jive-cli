a8c1eab89f2b9148e8fda1cb9c869e423d83d510
John Newbigin <john.newbigin@rea-group.com>
2016-05-19 14:15:32 +1000

    Generate markdown table from input .json file Still plenty of features to add

---
+++ json/json_table_to_md.php
@@ -0,0 +1,55 @@
+#!/usr/bin/php -f
+<?php
+
+$headings = array(
+	"name" 			=> "Account Name",
+	"email" 		=> "EMail",
+	"id"			=> "Account ID",
+	"alias"			=> "Account alias",
+	"custodian" 		=> "Custodian",
+	"direct_connect"	=> "Direct Connect",
+	"cidr_range" 		=> "CIDR Range",
+	"platform"		=> "Platform Image",
+	"shared"		=> "Shared or LoB",
+	"fedorated"		=> "Federated Identity",
+	"mfa"			=> "root MFA",
+	"cloudtrail"		=> "CloudTrail",
+	"awsconfig"		=> "AWS Config",
+	"cloudability"		=> "Cloudability",
+);
+
+$o = array();
+
+$a = "|";
+$b = "|";
+
+foreach($headings as $index => $heading)
+{
+	$a.= $heading."|";
+	$b.= str_repeat("-", strlen($heading))."|";
+}
+$o[] = $a;
+$o[] = $b;
+$filename = $_SERVER['argv'][1];
+$file = file_get_contents($filename, true);
+$json = json_decode($file);
+//print_r($json);
+foreach($json as $account)
+{
+	$line = "|";
+	foreach($headings as $index => $heading)
+	{
+		if(isset($account->$index))
+		{
+			$line.= $account->$index;
+		}
+		$line.= "|";
+	}
+	$o[] = $line;
+}
+
+foreach($o as $line)
+{
+	echo "$line\n";
+}
+?>
