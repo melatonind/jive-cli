f911e581ba41d5e6201743b22bda0cbed5bd6e17
Kimie Nakahara <kimie.nakahara@rea-group.com>
2016-05-19 16:37:53 +1000

    Limit debug to create_doc func. Automatically generates doc name.

--- lib/create_doc.sh
+++ lib/create_doc.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 #Stop on error
-set -o errexit
-set -o xtrace
 
 function create_doc {
 
   set_login
   set_password
-
+(
+set -o errexit
+set -o xtrace
 #Some TODO
 # instead of README, accept any md file
-# Define a name convention for subject i.e. document name
+# allow user to give a space in Jive for the document to be created
+# delete visibility hidden line
 # Prettiffy this script
 
 pandoc README.md > f1.tmp
@@ -19,12 +20,15 @@ sed -e 's/"/\\"/g' f1.tmp  > f2.tmp
 cat f2.tmp | tr -d '\012' > f3.tmp
 content=`cat f3.tmp`
 
+#assumes .MD file is in a git repo
+doc_name=`git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p'`
+
 curl -v -u "$USER_ID":"$USER_PW" \
      -k --header "Content-Type: application/json" \
      -d '{ "type": "document",
-           "subject": "Test document3",
+           "subject": "'"${doc_name}"' README",
            "visibility": "hidden",
-           "tags": [one, two, three],
+           "tags": [readme],
            "content":
               { "type": "text/html",
                 "text": "<body>'"${content}"'</body>"
@@ -33,5 +37,5 @@ curl -v -u "$USER_ID":"$USER_PW" \
      "https://community.rea-group.com/api/core/v3/contents"
 
 rm *.tmp
-
+)
 }
