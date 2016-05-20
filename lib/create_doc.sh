6ea254f129b6eb8406749ce977bc74a3d93eee1d
John Newbigin <john.newbigin@rea-group.com>
2016-05-20 11:43:58 +1000

    Use create_doc function and clean up everything else

--- lib/create_doc.sh
+++ lib/create_doc.sh
@@ -22,25 +22,20 @@ function create_doc {
   set_login
   set_password
 (
-set -o errexit
-set -o xtrace
+#set -o errexit
+#set -o xtrace
 #Some TODO
 # instead of README, accept any md file
 # allow user to give a space in Jive for the document to be created
 # delete visibility hidden line
 # Prettiffy this script
 
-echo -n "Pls enter your filename - must be in current dir, .md files only:"
-read filename
-pandoc ${filename}.md > f1.tmp
-sed -e 's/"/\\"/g' f1.tmp  > f2.tmp
-cat f2.tmp | tr -d '\012' > f3.tmp
-CONTENT=`cat f3.tmp`
+  convert_md
 
 #assumes .MD file is in a git repo
 REPO_NAME=`git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p'`
-
-curl -v -u "$USER_ID":"$USER_PW" \
+OUTPUT=$(mktemp -t jiveXXXX)
+curl -u "$USER_ID":"$USER_PW" \
      -k --header "Content-Type: application/json" \
      -d '{ "type": "document",
            "subject": "'"${REPO_NAME}"' '"${filename}"'",
@@ -48,11 +43,22 @@ curl -v -u "$USER_ID":"$USER_PW" \
            "tags": [readme],
            "content":
               { "type": "text/html",
-                "text": "<body>'"${CONTENT}"'</body>"
+                "text": '"${CONTENT}"'
               }
          }' \
-     "https://community.rea-group.com/api/core/v3/contents"
+     "${JIVE_ENDPOINT}/contents" > $OUTPUT
 
-rm f*.tmp
+  FILETYPE=$(file $OUTPUT)
+  if [ "${FILETYPE%% *}" = "gzip" ] ; then
+    zcat $OUTPUT | grep "<title>"
+  else
+    NEW_VERSION=$(cat $OUTPUT | jq -r '.id')
+    if [ "$NEW_VERSION" = "null" -o "$NEW_VERSION" = "" ] ; then
+      cat $OUTPUT | jq -r '.error.status, .error.message '
+    else
+      echo "Created DOC-${NEW_VERSION}"
+    fi
+  fi
+
 )
 }
